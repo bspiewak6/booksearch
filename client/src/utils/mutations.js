1a30d01744ed33cae7c7e8278e08071a9f994c9d
Spiewak <bspiewak6@gmail.com>
2021-01-17 14:48:30 -0500

    trying to fix log in bug

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -5,8 +5,8 @@ export const LOGIN_USER = gql`
     login(email: $email, password: $password) {
       token
       user {
+        _id
         username
-        email
       }
     }
   }
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -47,7 +47,7 @@ const resolvers = {
         if (context.user) {
           const updatedUser = await User.findByIdAndUpdate(
             { _id: context.user._id},
-            { $push: { saveBook: {bookData} } },
+            { $push: { savedBooks: {bookData} } },
             { new: true, runValidators: true }
           )
 
@@ -61,7 +61,7 @@ const resolvers = {
         if (context.user) {
           const updatedUser = await User.findByIdAndUpdate(
             { _id: context.user._id},
-            { $pull: { savedBook: bookId } },
+            { $pull: { savedBooks: bookId } },
             { new: true, runValidators: true }
           )
 
