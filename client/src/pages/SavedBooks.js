fbdb4d59f5bd228f94c521788649c79fd2200055
Spiewak <bspiewak6@gmail.com>
2021-01-18 20:44:56 -0500

    updated saveedbooks.js file with saved book for user functionality success

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -7,42 +7,14 @@ import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
 
 const SavedBooks = () => {
-  // const [userData, setUserData] = useState({});
-
-  // use this to determine if `useEffect()` hook needs to run again
-  // const userDataLength = Object.keys(userData).length;
-
-  // useEffect(() => {
-  //   const getUserData = async () => {
-  //     try {
-  //       const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-  //       if (!token) {
-  //         return false;
-  //       }
-
-  //       const response = await getMe(token);
-
-  //       if (!response.ok) {
-  //         throw new Error('something went wrong!');
-  //       }
-
-  //       const user = await response.json();
-  //       setUserData(user);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-
-  //   getUserData();
-  // }, [userDataLength]);
 
   const [removeBook] = useMutation(REMOVE_BOOK);
   
   const { loading, data } = useQuery(GET_ME);
+  console.log(data);
 
-  const userData = data?.userData || [];
-  console.log(userData);
+  const userData = data?.me || [];
+  console.log(userData.savedBooks);
   
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -84,16 +56,12 @@ const SavedBooks = () => {
       </Jumbotron>
       <Container>
         <h2>
-          {/* {userData.savedBooks.length
+          {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
-            : 'You have no saved books!'} */}
-            {userData
-            ? `Viewing ${userData} saved ${userData === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
         <CardColumns>
-          {/* {userData.savedBooks.map((book) => { */}
-            {userData.map((book) => {
+          {userData.savedBooks.map((book) => {
             return (
               <Card key={book.bookId} border='dark'>
                 {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
